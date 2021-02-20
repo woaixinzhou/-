898f76d5404e0559ee1c8d447300d2154d5da6f7
canghaiyishengxiao2021 <79012961+canghaiyishengxiao2021@users.noreply.github.com>
2021-02-20 21:20:52 +0800

    Update Ponysitters_Club_Season.js

--- Ponysitters_Club_Season.js
+++ Ponysitters_Club_Season.js
@@ -63,7 +63,7 @@ async function changeFiele(content, cookie) {
      newContent = newContent.replace(Efork,'Efork');
     
     newContent = newContent.replace(/require\('.\/(\w+)ShareCodes.js\'\)/g, JSON.stringify(shareCodes)); 
-    newContent =newContent.replace(/var Key = ''/, `var Key = `+cookie);
+    newContent =newContent.replace(/var Key = ''/, `var Key = `+cookie);`var Key = '${cookie}'`);
  
    
      
